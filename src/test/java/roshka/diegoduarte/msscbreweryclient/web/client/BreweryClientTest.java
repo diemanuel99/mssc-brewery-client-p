54f4745ba3b6b107d3e48ff9dec70779e72f36c2
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-03-17 10:07:22 -0300

    tests por requests

--- src/test/java/roshka/diegoduarte/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/roshka/diegoduarte/msscbreweryclient/web/client/BreweryClientTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import roshka.diegoduarte.msscbreweryclient.web.model.BeerDto;
+import roshka.diegoduarte.msscbreweryclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -24,5 +25,35 @@ class BreweryClientTest {
         URI uri = client.saveNewBeer(beerDto);
         assertNotNull(uri);
         System.out.println(uri);
+    }
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+    @Test
+    void testDeleteBeer(){
+        client.deleteBeer(UUID.randomUUID());
+    }
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        URI uri = client.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+    @Test
+    void testDeleteCustomer(){
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
